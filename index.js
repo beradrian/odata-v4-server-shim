fa8148e90a9da981f49761988c3bf190b1be9cce
Adrian Ber <beradrian@yahoo.com>
2020-03-28 00:34:35 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+function NoOp(options) {
+    return function (object, propertyName) {
+    };
+}
+
+exports.Edm = {
+  Binary: NoOp,
+  Boolean: NoOp,
+  Byte: NoOp,
+  Date: NoOp,
+  DateTimeOffset: NoOp,
+  Decimal: NoOp,
+  Double: NoOp,
+  Duration: NoOp,
+  Guid: NoOp,
+  Int16: NoOp,
+  Int32: NoOp,
+  Int64: NoOp,
+  SByte: NoOp,
+  Single: NoOp,
+  Stream: NoOp,
+  String: NoOp,
+  TimeOfDay: NoOp,
+  Geography: NoOp,
+  GeographyPoint: NoOp,
+  GeographyLineString: NoOp,
+  GeographyPolygon: NoOp,
+  GeographyMultiPoint: NoOp,
+  GeographyMultiLineString: NoOp,
+  GeographyMultiPolygon: NoOp,
+  GeographyCollection: NoOp,
+  Geometry: NoOp,
+  GeometryPoint: NoOp,
+  GeometryLineString: NoOp,
+  GeometryPolygon: NoOp,
+  GeometryMultiPoint: NoOp,
+  GeometryMultiLineString: NoOp,
+  GeometryMultiPolygon: NoOp,
+  GeometryCollection: NoOp
+};
